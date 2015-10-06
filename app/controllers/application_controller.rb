7ec5cfc67ec8e4813da5653065e2ca85391accf2
Luigi <luisvillasenor@yahoo.com>
2015-10-06 03:18:31 +0000

    cambio de mensaje hello por hola

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text: "Hello, Word!"
+    render text: "Hola, Mundo!"
   end
 
 end
